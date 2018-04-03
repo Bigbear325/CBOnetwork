ee4c9173f4173f7957f4a321d7e9e2b598e71911
zfree <zfree@gmail.com>
2018-04-03 16:45:59 -0400

    local proxy v0.1
    start to write local proxy

---
+++ twitpy/client/nproxy.py
@@ -0,0 +1,20 @@
+import socket
+import select
+import time
+import sys
+
+
+class forward(object):
+	"""this will be the twitter post method"""
+	def __init__(self, arg):
+		super(forward, self).__init__()
+		self.arg = arg
+
+
+
+class theProxy(object):
+	"""this will be the """
+	def __init__(self, arg):
+		super(theProxy, self).__init__()
+		self.arg = arg
+		
