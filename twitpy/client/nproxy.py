45741de9d98605870725b27015bc5c468d95859c
zfree2018 <zfree2018@gmail.com@gmail.com>
2018-04-05 15:38:12 -0400

    proxy v0.14

--- twitpy/client/nproxy.py
+++ twitpy/client/nproxy.py
@@ -7,46 +7,48 @@ import cipher
 from random import *
 
 class twitsend:
-	"""this will be the twitter post method
-		
-		note:
+    """this will be the twitter post method
+        note:
 		option 1. will directly create twit connection. 
 		just for posting, will talk about getting(streaming) later
 
 		option 2. creat socket. but I'm not sure
-	"""
+    """
 
-	def __init__(self):
-                auth = tweepy.OAuthHandler(cipher.consumer_key, cipher.consumer_secret)
-		auth.set_access_token(cipher.access_token, cipher.access_token_secret)
-		self.api = tweepy.API(auth)
+    def __init__(self):
+        auth = tweepy.OAuthHandler(cipher.consumer_key, cipher.consumer_secret)
+        auth.set_access_token(cipher.access_token, cipher.access_token_secret)
+        self.api = tweepy.API(auth)
 
-	def start(self, content):
-		self.api.update_status('content->' + str(random() * 100)+ content)
-		print "it works"
+    def start(self, content):
+	self.api.update_status('content->' + str(random() * 100)+ content)
+	print "it works"
 
 class TheProxy:
-	def __init__(self, host, port):
-		self.input_list = []
+    def __init__(self, host, port):
+	self.input_list = []
+	self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	self.server.bind((host, port))
+	self.server.listen(200)
+        print('listen to host' + str(host) + ',port' + str(port))
 
-		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 123)
-		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.server.bind((host, port))
-		self.server.listen(200)
+    def main_loop(self):
+	self.input_list.append(self.server)
+        while 1:
+            time.sleep(1)
+            content = 'input:' + str(self.input_list)
+	    #print(content)
+	    self.on_accept(content)
 
-	def main_loop(self):
-		self.input_list.append(self.server)
-		content = 'input:' + str(self.input_list)
-		print(content)
-		self.on_accept(content)
-
-	def on_accept(self,content):
-		twit = twitsend().start(content)
+    def on_accept(self,content):
+        print(content)
+        #twit = twitsend().start(content)
 
 if __name__ == '__main__':
-	proxy = TheProxy('', 9090)
-	try:
-		proxy.main_loop()
-	except KeyboardInterrupt:
-		print "Ctrl C - Stopping server"
-		sys.exit(1)
+    proxy = TheProxy('127.0.0.1', 9090)
+    try:
+        proxy.main_loop()
+    except KeyboardInterrupt:
+	print "Ctrl C - Stopping server"
+	sys.exit(1)
