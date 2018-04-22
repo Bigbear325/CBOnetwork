cae050b4fc1e1d3fa71ccb7daf680280a1ad2aed
ZHLIU <liu.cmri@gmail.com>
2018-04-22 17:55:57 -0400

    qrcode v0.12

--- pyCharm/qrcode_generator.py
+++ pyCharm/qrcode_generator.py
@@ -1,14 +1,6 @@
-import qrcode
-import qrcode.image.svg
+''' this code will get in a file, read it, devide it into
+    pieces and covert it using base64, then generate multi qrcode
+'''
 
-if method == 'basic':
-    # Simple factory, just a set of rects.
-    factory = qrcode.image.svg.SvgImage
-elif method == 'fragment':
-    # Fragment factory (also just a set of rects)
-    factory = qrcode.image.svg.SvgFragmentImage
-else:
-    # Combined path factory, fixes white space that may occur when zooming
-    factory = qrcode.image.svg.SvgPathImage
 
-img = qrcode.make('Some data here', image_factory=factory)
+
