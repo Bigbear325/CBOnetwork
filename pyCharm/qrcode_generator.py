a0e3150a13274da9ef69a2c040fb4b6b3cfa20f4
ZHLIU <liu.cmri@gmail.com>
2018-04-22 23:54:13 -0400

    qrcode v0.21

--- pyCharm/qrcode_generator.py
+++ pyCharm/qrcode_generator.py
@@ -2,5 +2,56 @@
     pieces and covert it using base64, then generate multi qrcode
 '''
 
+from PIL import Image
+import os, sys
+import math
+
+def gen_pic(ori_file, save_file):
+    pic = open(ori_file, 'rb')
+    bi = pic.read()
+
+    #get the length of bi
+    bi_len = len(bi)
+
+    # convert to hex str
+    hex_len = "%0*x" % (8, bi_len)
+
+    # convert hex to str
+    str_len = ""
+    for i in range(0, 8, 2):
+        cur = int(hex_len[i:i + 2], 16)
+        str_len += chr(cur)
+
+    bi = str_len + bi
+
+    # pic size 720p Resolution: 1280x720
+    #width = int(math.sqrt(bi_len))
+    width = 426
+    # add something to the end
+    end_str = ""
+    for i in range(bi_len):
+        end_str += 'a'
+
+    bi = bi + end_str
+
+    height = 240
+    #img = Image.frombytes('L', (width, bi_len / width + 1), bi)
+    img = Image.frombytes('L', (width, height), bi)
+    img.save(save_file)
+
+ori_file_dir = "/Users/lzhmbp/Documents/GitHub/CBOnetwork/twitpy/client/in"
+save_file_dir = "/Users/lzhmbp/Documents/GitHub/CBOnetwork/twitpy/client/out"
+
+files = os.listdir(ori_file_dir)
+file_names = []
+for f in files:
+    file_names.append(f)
+
+num_files = len(file_names)
+num_finished = 0
+for f in file_names:
+    num_finished += 1
+    gen_pic(ori_file_dir + '/' + f, save_file_dir + '/' + f + '.png')
+    print str(num_finished) + " out of " + str(num_files) + " finished."
 
 
